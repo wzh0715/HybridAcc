561c0c7868c6b03b3b6ebfbe26e452fb83e33160
YuK1 <95479964+wzh0715@users.noreply.github.com>
2025-03-11 20:20:47 +0800

    update

--- SDA/solution1/.autopilot/db/ip_tmp/run_vivado_gen_ip.tcl
+++
@@ -1,9 +0,0 @@
-create_project prj -part xcvu35p_CIV-fsvh2892-3-e -force
-set_property target_language verilog [current_project]
-set vivado_ver [version -short]
-set COE_DIR "../../syn/verilog"
-source "D:/Download/SDA/SDA/solution1/syn/verilog/top_fadd_32ns_32ns_32_2_full_dsp_1_ip.tcl"
-source "D:/Download/SDA/SDA/solution1/syn/verilog/top_fdiv_32ns_32ns_32_6_no_dsp_1_ip.tcl"
-source "D:/Download/SDA/SDA/solution1/syn/verilog/top_fmul_32ns_32ns_32_2_max_dsp_1_ip.tcl"
-source "D:/Download/SDA/SDA/solution1/syn/verilog/top_fsqrt_32ns_32ns_32_6_no_dsp_1_ip.tcl"
-source "D:/Download/SDA/SDA/solution1/syn/verilog/top_fsub_32ns_32ns_32_2_full_dsp_1_ip.tcl"
--- SDA/solution1/sim/verilog/sim.bat
+++
@@ -1,1 +0,0 @@
-    D:/Applications/Xilinx/Vitis_HLS/2023.2/bin/vitis_hls run_sim.tcl
